4386c068421dd961fe157bb1ee7f6e5b29b96f97
NajiyaShabeer <najishab@gmail.com>
2023-08-08 11:33:33 +0530

    first commit

--- assignnew.js
+++ assignnew.js
@@ -1,45 +1,46 @@
 //Write a Javascript function to get an array and to read the first element of an array.Check whether
 // it is prime or not.
 
-function Prime(num)
+function prime(n)
 {
-    if (num<=1);
+    if (n<=1)
     {
     return 'Not prime';
     }
-}
-for(let i=2;i<=Math.sqrt(num);i++)
+
+for(let k=2;k<=Math.sqrt(n);k++)
 {
- if (num%i===0);
+ if (n%k===0)
  {
-    return 'Not prime';
+    return ' Not prime';
  }
 }
  return 'Yes,it is prime';
+}
 
 function CheckFirst(arr)
 {
     const f=arr[0];
-    return Prime(f);
+    return prime(f);
 }
 const numbers=[7,9,8,3,5,6];
-console.log("First item is" + numbers[0]+',' + CheckFirst(numbers));
+console.log("First item is" + " " + numbers[0]+',' + CheckFirst(numbers));
 
 // Write a Javascript program to find the most frequent item of an array
-var array2=[5,6,8,6,6,2,3,6];
+var array1=[5,6,8,6,6,2,3,6];
 var mf=1;
 var m=0;
 var item;
-for (var i=0;i<array2.length;i++)
+for (var i=0;i<array1.length;i++)
 {
-    for (var j=1;j<array2.length;j++)
+    for (var j=1;j<array1.length;j++)
     {
-        if (array2[i]==array2[j])
+        if (array1[i]==array1[j])
         m++;
     if (mf<m)
     {
         mf=m;
-        item=array2[i];
+        item=array1[i];
     }
     }
     m=0;
@@ -66,4 +67,17 @@ else
     console.log(x + " " + "is odd");
 }
 }
+//Write a Javascript program to find the sum of the squares of the elements of an array
+
+function squaresum(array2)
+{
+var sum=0;
+let i=array2.length;
+while(i--)
+
+    sum+=Math.pow(array2[i],2)
+    return sum;
+}
+console.log(squaresum([2,4,6,8,10]))
+
 
